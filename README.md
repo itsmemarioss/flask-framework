2b9f58c4022984a8f858d10eab9f628ad14dce3a
Mario Sousa <m_santos_sousa@hotmail.com>
2021-01-10 14:08:01 +0000

    Add a form to fetch data from alphavantage

--- README.md
+++ README.md
@@ -39,3 +39,21 @@ A [finished example](https://lemurian.herokuapp.com) that demonstrates some basi
   and [examples](https://github.com/bokeh/bokeh/tree/master/examples/embed).
 - Make the plot visible on your website through embedded HTML or other methods - this is where Flask comes in to manage the interactivity and display the desired content.
 - Some good references for Flask: [This article](https://realpython.com/blog/python/python-web-applications-with-flask-part-i/), especially the links in "Starting off", and [this tutorial](https://github.com/bev-a-tron/MyFlaskTutorial).
+
+## How to run
+
+Create the environment
+
+    python3 -m venv venv
+
+Activate the environment
+
+    venv\Scripts\activate
+
+Install  dependencies 
+
+    pip install flask simplejson requests
+
+Run app
+
+    flask run
--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect
+import simplejson as json
+import requests
 
 app = Flask(__name__)
 
@@ -9,6 +11,27 @@ def index():
 @app.route('/about')
 def about():
   return render_template('about.html')
+
+#new route to handle the request from index page 
+@app.route('/print', methods=['POST'])
+def print_symbol():
+  
+  #retrieve the value of the symbol informed by the user
+  symbol = request.form['symbol']
+  
+  #format the url adding the symbol parameter to it
+  url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&apikey=NRZTU4B1MMEPE0EJ&datatype=json'.format(symbol)
+
+  #request the json and load it
+  page = requests.get(url)
+  obj = json.loads(page.content)
+
+  #after loading json into a variable it will create a dictionary so you can add the properties of the json by name
+  print(obj.__class__) #dict
+
+  #return only the dates with the prices part
+  #from now on you need to clean the data to use it in pandas. I suppose ;)
+  return obj["Time Series (Daily)"]
 
 if __name__ == '__main__':
   app.run(port=33507)
